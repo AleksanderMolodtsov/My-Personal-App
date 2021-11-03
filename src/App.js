dc644f4c230719914644f3b448369693979d0839
AleksanderMolodtsov <rabotaallday163@gmail.com>
2021-11-03 15:52:32 +0400

    my first pull request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://my-personal-app-e3666-default-rtdb.europe-west1.firebasedatabase.app/test.json')
+      const data = await  response.json()
+
+      console.log(data,'data')
+    }
+
+    fetchData();
+  }, [])
+
+  const onClick = async () => {
+    const data = {test:'fuuuuuck'};
+    const response = await fetch('https://my-personal-app-e3666-default-rtdb.europe-west1.firebasedatabase.app/.json', {
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+    const dataFromResponse = await response.json()
+
+    console.log('dataFromResponse', dataFromResponse)
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +39,8 @@ function App() {
         >
           Learn React
         </a>
+        <p>My first pull request! MTFKR</p>
+        <button onClick={onClick}>change data</button>
       </header>
     </div>
   );
